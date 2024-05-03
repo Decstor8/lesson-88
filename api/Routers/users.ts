b9067706df73a36e8077dd54d4334d30071d79ca
Maksim Tkashev <maks93makula@gmail.com>
2024-05-03 15:32:06 +0300

    Создан роут Users и модель к нему, можно зарегистрироваться и при post запросе на users - sessions логин

---
+++ api/Routers/users.ts
@@ -0,0 +1,54 @@
+import {Router} from "express";
+import User from "../models/User";
+import mongoose, {mongo} from "mongoose";
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (req, res, next) => {
+    try {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        user.generateToken();
+        await user.save();
+        return res.send({message: 'ok!', user});
+    } catch (err) {
+        if (err instanceof mongoose.Error.ValidationError) {
+            return res.status(422).send(err);
+        }
+
+        if (err instanceof mongo.MongoServerError && err.code === 11000) {
+            return res.status(422).send({message: 'Имя должно быть уникальным..'});
+        }
+
+        next(err);
+    }
+});
+
+usersRouter.post('/sessions', async (req, res, next) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+
+        if(!user) {
+            return res.status(422).send({error: 'Имя пользователя или пароль не удалось найти'});
+        }
+
+        const passwordValid = await user.checkPassword(req.body.password);
+
+        if(!passwordValid) {
+            return res.status(422).send({error: 'Имя пользователя или пароль не удалось найти'});
+        }
+
+        user.generateToken();
+        await user.save();
+
+        return res.send({message: 'Имя пользователя и пароль верны!', user});
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+export default usersRouter;
